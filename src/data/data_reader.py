f70a425f404849de923397b876d475c8a8038035
Jenne Van Veerdeghem <jennevv@gmail.com>
2024-09-16 15:51:48 +0200

    Add data_reader.py module and tests

---
+++ src/data/data_reader.py
@@ -0,0 +1,61 @@
+import joblib
+import pandas as pd
+
+
+class DataReader:
+    """
+    Class for reading data from either a pickle or csv file.
+    The processed dataset must be a pandas DataFrame.
+    """
+
+    def __init__(self):
+        pass
+
+    def _read_csv(self, file) -> pd.DataFrame:
+        data = pd.read_csv(file)
+        return data
+
+    def _read_pickle(self, file) -> pd.DataFrame:
+        pickle = joblib.load(file)
+        data = pickle['processed_dataset']
+
+        if not isinstance(data, pd.DataFrame) and not isinstance(data, pd.Series):
+            raise NotImplementedError(f'The processed dataset in the pickle file is not a pandas DataFrame. Instead got {type(data)}')
+
+        return data
+
+    def read_data(self, file, file_type) -> pd.DataFrame:
+        """
+        Parameters
+        ----------
+        file : str
+            The path to the file to read.
+        file_type : str
+            The type of file to read. Either 'csv' or 'pickle'.
+
+        Returns
+        -------
+        pd.DataFrame
+            The data read from the file
+
+        Raises
+        ------
+        ImportError
+            If the file is not of the file type specified.
+        NotImplementedError
+            If the file type specified is not implemented or the processed dataset in the pickle file is not a pandas DataFrame.
+        """
+        if file_type == 'csv':
+            try:
+                data = self._read_csv(file)
+            except:
+                raise ImportError(f'The file is not of the file type {file_type}.')
+        elif file_type == 'pickle':
+            try:
+                data = self._read_pickle(file)
+            except:
+                raise ImportError(f'The file is not of the file type {file_type}.')
+        else:
+            raise NotImplementedError(f'The file type {file_type} is not implemented. Choose either csv or pickle.')
+
+        return data
