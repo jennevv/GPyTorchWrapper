d8097f4351ceffb3ccac3526a5612d988c5ea406
jennevv <89158460+jennevv@users.noreply.github.com>
2025-06-11 14:28:48 +0200

    Add logger to timer globally

--- gpytorchwrapper/src/utils/timer.py
+++ gpytorchwrapper/src/utils/timer.py
@@ -1,24 +1,35 @@
+import logging
 import time
+
+logger = logging.getLogger(__name__)
 
 
 class Timer:
-    def __init__(self, logger):
-        self.t1 = (0., 0.)
-        self.t2 = (0., 0.)
-        self.logger = logger
+    def __init__(self, step_name: str):
+        self.t1 = (0.0, 0.0)
+        self.t2 = (0.0, 0.0)
+        self.step_name = step_name
+
     @staticmethod
     def _time() -> tuple[float, float]:
         return time.perf_counter(), time.process_time()
+
     def set_init_time(self):
         self.t1 = self._time()
+
     def set_final_time(self):
         self.t2 = self._time()
+
     def real_time(self):
         return self.t2[0] - self.t1[0]
+
     def cpu_time(self):
         return self.t2[1] - self.t1[1]
-    def log_timings(self, step_name: str):
-        self.logger.info(f"------------------------------------------\nTIMINGS FOR {step_name.upper()}")
-        self.logger.info(f"Real time: {self.real_time():.2f} seconds")
-        self.logger.info(f"CPU time: {self.cpu_time():.2f} seconds")
-        self.logger.info("------------------------------------------\n")
+
+    def log_timings(self):
+        logger.info(
+            f"------------------------------------------\nTIMINGS FOR {self.step_name.upper()}"
+        )
+        logger.info(f"Real time: {self.real_time():.2f} seconds")
+        logger.info(f"CPU time: {self.cpu_time():.2f} seconds")
+        logger.info("------------------------------------------\n")
